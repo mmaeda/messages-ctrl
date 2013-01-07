055164aef0e3a0d5160d478500e043b84e763843
Masayuki Maeda <mmae122@gmail.com>
2013-01-08 02:06:21 +0900

    Chapter24.1.1 340ページ。rails new、Gemfile、.gitignoreをコピーした直後。まだ generate はやってない。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MessagesController::Application.config.secret_token = 'e1a1b36eeb937cf842fc720c7cde7546e97c34f8ca2cf3e9acd02dd59d119c0458fda1dfce368366aa172ca198dae67058b6cfe18ff7dc65734ceadfa3dce29e'
